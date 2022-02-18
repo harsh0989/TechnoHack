7986e1b46645a4b5a5ea39f72e88619f74906a16
harsh0989 <harshmangukiya1506@gmail.com>
2022-02-19 04:21:16 +0530

    Dashboard created

--- technohackfrontend/src/App.js
+++ technohackfrontend/src/App.js
@@ -1,17 +1,19 @@
-import { useEffect } from 'react';
 import './App.css';
 import Splashpage from './pages/Splashpage'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
+import Navbar from './components/Navbar';
 function App() {
   return (
-
+    
     <Router>
+      <Navbar style={{position:'absolute',zIndex:'1'}} />
       <Routes>
         <Route exact path='/' element={<Splashpage />} />
         <Route exact path='/login' element={<Login />} />
       </Routes>
     </Router>
+
   )
 
 }
